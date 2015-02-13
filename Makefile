adb9c36f421f7e5487572fff38f1c0744664e9b6
Ze Mao <zemao@lvl-mac033.usc.edu>
2015-02-12 20:22:19 -0800

    can run 1 thread

--- Makefile
+++ Makefile
@@ -2,3 +2,4 @@ default:
 	g++ -std=c++11 Timer.cpp -c -o Timer.o
 	g++ -std=c++11 main.cpp -c -o main.o
 	g++ -std=c++11 Timer.o main.o -o lab11.o
+	./lab11.o
--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <iomanip>
 #include <cmath>
 #include <thread>
+#include <vector>
 #include "Timer.h"
 using namespace std;
 // InitValues - set array values.  Array items are
@@ -15,13 +16,12 @@ using namespace std;
 // first - index of first array item to set
 // count - number of items to set
 void InitValues(double *a, int first, int count) {
-  cout << "thread begin" << endl;
-  int limit = first + count;
-  for (int i = first; i < limit; ++i) {
-    a[i] = sqrt((double) i);
-    cout << i << endl;
-  }
-  cout << "thread end" << endl;
+    cout << "init thread begin" << endl;
+    int limit = first + count;
+    for (int i = first; i < limit; ++i) {
+        a[i] = sqrt((double) i);
+    }
+    cout << "init thread end" << endl;
 }
 
 // SumValues - sum array values.
@@ -31,12 +31,14 @@ void InitValues(double *a, int first, int count) {
 // first - index of first array item to be summed
 // count - number of items to sum
 void SumValues(const double *a, double *sum, int first, int count) {
-  double sumTmp = 0.0;
-  int limit = first + count;
-  for (int i = first; i < limit; ++i) {
-    sumTmp += a[i];
-  }
-  *sum = sumTmp;
+    cout << "sum thread begin" << endl;
+    double sumTmp = 0.0;
+    int limit = first + count;
+    for (int i = first; i < limit; ++i) {
+        sumTmp += a[i];
+    }
+    *sum = sumTmp;
+    cout << "sum thread end" << endl;
 }
 
 // ThreadedSum - initialize and sum a large array using
@@ -45,63 +47,75 @@ void SumValues(const double *a, double *sum, int first, int count) {
 // numThreads - number of threads to use
 // arraySize - size of array to use
 double ThreadedSum(int numThreads, size_t arraySize) {
-  double sum = 0.0;
-  double *a = NULL;
-  static bool init=false;
-  std::thread initThread;
+    double sum = 0.0;
+    static double *a = NULL;
+    static bool init=false;
+    std::thread initThread;
+    vector<std::thread> sumThreadsVector;
 
-  if(init == 0){
-    //do init
-    a = new double(arraySize);
-    cout << "initialize array, total size " << arraySize << endl;
-    for(int i=0; i<numThreads; i++){
-      initThread = std::thread(InitValues, a, 0, arraySize);
+    if(init == false){
+        //do init
+        a = new double[arraySize];
+        cout << "initialize array, total size " << arraySize << endl;
+        for(int i=0; i<numThreads; i++){
+            initThread = std::thread(InitValues, a, 0, arraySize);
+        }
+        initThread.join();
+        init = true;
+        return 0;
+
+    }else{
+        double sumI[numThreads];
+        for(int i=0; i<1; i++){
+            sumThreadsVector.push_back(std::thread(SumValues, a, &sumI[i], 0, arraySize));
+        }
+        for(auto &th: sumThreadsVector){
+            th.join(); 
+        }
+        for(int i=0; i<numThreads; i++){
+            sum+=sumI[i]; 
+        } 
+        // undo init
+        delete a;
+        init = false;
+        return sum;
     }
-    initThread.join();
-    init = true;
-    return 0;
 
-  }else{
-    return 0;
-    delete a;
-    init = false;
-  }
+    // ********************
+    // Write your code here
+    // ********************
 
-  // ********************
-  // Write your code here
-  // ********************
-
-  //return sum;
+    //return sum;
 }
 
 int main(int argc, const char * argv[])
 {
-  // Use the number of hardware threads provided by the CPU(s).
-  // If only one hardware thread, use two threads.
-  int numThreads = std::thread::hardware_concurrency();
-  std::cout << "Hardware threads available: " << numThreads << std::endl;
-  if (numThreads < 2) numThreads = 2;  // use at least 2 threads
+    // Use the number of hardware threads provided by the CPU(s).
+    // If only one hardware thread, use two threads.
+    int numThreads = std::thread::hardware_concurrency();
+    std::cout << "Hardware threads available: " << numThreads << std::endl;
+    if (numThreads < 2) numThreads = 2;  // use at least 2 threads
 
-  // Create a Timer object
-  Timer t;
+    // Create a Timer object
+    Timer t;
 
-  // Run for each power of 2 from 20 to 26 (You may want to start
-  // with 10 to 15 for testing)
-  for (int power = 20; power <= 20; power++)
-  {
-    t.StartTimer();
-    double sum1 = ThreadedSum(1, 1<<power);
-    t.EndTimer();
-    std::cout << "Elapsed time: " << t.GetElapsedTime() << ", "
-              << "arraySize " << (1<<power) << ", numThreads = 1, sum = "
-              << std::setprecision(15) << sum1 << std::endl;
+    // Run for each power of 2 from 20 to 26 (You may want to start
+    // with 10 to 15 for testing)
+    for (int power = 20; power <= 20; power++)
+    {
+        t.StartTimer();
+        double sum1 = ThreadedSum(1, 1<<power);
+        t.EndTimer();
+        std::cout << "Elapsed time: " << t.GetElapsedTime() << ", "
+            << "arraySize " << (1<<power) << ", numThreads = 1, sum = "
+            << std::setprecision(15) << sum1 << std::endl;
 
-    t.StartTimer();
-    double sumN = ThreadedSum(numThreads, 1<<power);
-    t.EndTimer();
-    std::cout << "Elapsed time: " << t.GetElapsedTime() << ", "
-              << "arraySize " <<(1<<power)<< ", numThreads = "
-              << numThreads << ", sum = "
-              << std::setprecision(15) << sumN << std::endl;
-  }
+        t.StartTimer();
+        double sumN = ThreadedSum(numThreads, 1<<power);
+        t.EndTimer();
+        std::cout << "Elapsed time: " << t.GetElapsedTime() << ", "
+            << "arraySize " <<(1<<power)<< ", numThreads = "
+            << numThreads << ", sum = "
+            << std::setprecision(15) << sumN << std::endl;
+    }
 }
